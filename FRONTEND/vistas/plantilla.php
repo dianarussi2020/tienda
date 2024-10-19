bbce4e4acbec11d419f77b0e7b6491690c546121
dianarussi2020 <diana.russi@udea.edu.co>
2024-10-18 23:19:44 -0500

    Configuración frontend y backend MVC

---
+++ FRONTEND/vistas/plantilla.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tienda virtual</title>
+</head>
+<body>
+    <h1>Comenzando el dllo</h1>
+</body>
+</html>
