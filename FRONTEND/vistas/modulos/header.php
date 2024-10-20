307e16a647c0b0e27e658fa7b0c74a1d49db5008
dianarussi2020 <diana.russi@udea.edu.co>
2024-10-19 22:29:35 -0500

    Maquetando cabecera

---
+++ FRONTEND/vistas/modulos/header.php
@@ -0,0 +1,147 @@
+<!-- 1. TOP -->
+<div class="container-fluid barraSuperior" id="top">
+    <div class="container">
+        <div class="row">
+            <!-- Iconos redes sociales -->
+            <div class="col-lg-9 col-md-9 col-sm-8 col-xs-12 social">
+                <ul>
+                    <li>
+                        <a href="#" target="_blank">
+                            <i class="fa fa-facebook redSocial facebookBlanco"
+                                aria-hidden="true">
+                            </i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" target="_blank">
+                            <i class="fa fa-youtube redSocial youtubeBlanco"
+                                aria-hidden="true">
+                            </i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" target="_blank">
+                            <i class="fa fa-twitter redSocial twitterBlanco"
+                                aria-hidden="true">
+                            </i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" target="_blank">
+                            <i class="fa fa-google-plus redSocial googleBlanco"
+                                aria-hidden="true">
+                            </i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" target="_blank">
+                            <i class="fa fa-instagram redSocial instagramBlanco"
+                                aria-hidden="true">
+                            </i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <!-- Crear cuenta e ingresar -->
+            <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 registro">
+                <ul>
+                    <li><a href="#modalIngreso" data-toggle="modal">Ingresar</a></li>
+                    <li>|</li>
+                    <li><a href="#modalRegistro" data-toggle="modal">Crear una cuenta</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- 2. HEADER -->
+<header class="container-fluid">
+    <div class="container">
+        <div class="row" id="cabezote">
+            <!-- 2.1 LOGOTIPO -->
+            <div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="logotipo">
+                <a href="#">
+                    <img src="../../../BACKEND/vistas/img/plantilla/logo.png"
+                        class="img-responsive">
+                </a>
+            </div>
+            <!-- 2.2 BLOQUE CATEGORIA Y BUSCADOR  -->
+            <div class="col-lg-6 col-md-6 col-sm-8 col-xs-12">
+                <!-- 2.2.1 Boton categorias -->
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 backColor" id="btnCategorias">
+                    <p>CATEGORIAS
+                        <span class="pull-right"></span>
+                        <i class="fa fa-bars" aria-hidden="true"></i>
+                    </p>
+                </div>
+                <!-- 2.2.2 Buscador -->
+                <div class="input-group col-lg-8 col-md-8 col-sm-8 col-xs-12 backColor" 
+                    id="buscador">
+                    <input type="search" name="buscar" 
+                        class="form-control" placeholder="Buscar...">
+                    <span class="input-group-btn">
+                        <a href="#">
+                            <button class="btn btn-default backColor" type="submit">
+                                <i class="fa fa-search"></i>
+                            </button>
+                        </a>
+                    </span>
+                </div>
+            </div>
+            <!-- 2.3 CARRITO DE COMPRAS  -->
+            <div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="carrito">
+                <a href="#">
+                    <button class="btn btn-default pull-left backcolor">
+                        <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                    </button>
+                </a>
+                <p>TU CESTA 
+                    <span class="cantidadCesta">3</span>
+                    <br/> USD 
+                    <span class="sumaCesta">20</span>
+                </p>
+            </div>
+        </div>
+        <!-- CATEGORIAS -->
+        <div class="col-xs-12 backColor" id="categorias">
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                <h4>
+                    <a href="#" class="pixelCategorias">Lorem Ipsum</a>
+                </h4>
+                <hr>
+                <ul>
+                   <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                   <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                   <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                   <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                   <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li> 
+                </ul>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                <h4>
+                    <a href="#" class="pixelCategorias">Lorem Ipsum</a>
+                </h4>
+                <hr>
+                <ul>
+                   <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                   <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                   <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                   <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                   <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li> 
+                </ul>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                <h4>
+                    <a href="#" class="pixelCategorias">Lorem Ipsum</a>
+                </h4>
+                <hr>
+                <ul>
+                   <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                   <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                   <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                   <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                   <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li> 
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
--- FRONTEND/vistas/plantilla.php
+++ FRONTEND/vistas/plantilla.php
@@ -3,9 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="title" content="Tienda Virtual">
+    <meta name="description" content="Tienda virtual desarrollada con php">
+    <meta name="keywords" content="">
     <title>Tienda virtual</title>
+    <!-- Hojas de estilo -->
+    <!-- Bootstrap v3.3.7 -->
+    <link rel="stylesheet" href="vistas/css/plugins/bootstrap.min.css">
+    <!--Font Awesome 4.6.1 -->
+    <link rel="stylesheet" href="vistas/css/plugins/font-awesome.min.css">
+    <!--Font google APIS -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu|Ubuntu+Condensed">
+
+    <!-- JS -->
+    <!-- jQuery v3.2.1 -->
+     <script src="vistas/js/plugins/jquery.min.js"></script>
+    <!-- Bootstrap v3.3.7 -->
+    <script src="vistas/js/plugins/bootstrap.min.js"></script>
 </head>
 <body>
-    <h1>Comenzando el dllo</h1>
+    <?php
+    /** HEADER */ 
+    include "modulos/header.php";
+    ?>
 </body>
 </html>
